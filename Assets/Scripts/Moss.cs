3357b4b888b01e5fd0179ece8e32f1dfb342d93a
tolkwes <tolkwes@gmail.com>
2017-01-21 13:07:15 -0800

    Moss Update
    Moss code is updated to account for overlapping moss objects.

--- Assets/Scripts/Moss.cs
+++ Assets/Scripts/Moss.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Moss : MonoBehaviour {
     public float dragIncrease;
+    private Dictionary<GameObject, float> initialDrags = new Dictionary<GameObject, float>();
 
 	void Start() {
-		
+        GameObject[] trashObjects = GameObject.FindGameObjectsWithTag("Trash");
+        for(int i = 0; i < trashObjects.Length; i++) {
+            float drag = trashObjects[i].GetComponent<Rigidbody2D>().drag;
+            initialDrags.Add(trashObjects[i], drag);
+        }
 	}
 	
 	void Update() {
@@ -16,14 +21,16 @@ public class Moss : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D c) {
         if(c.gameObject.tag == "Trash") {
             Rigidbody2D rb = c.GetComponent<Rigidbody2D>();
-            rb.drag += dragIncrease;
+            if(rb.drag == initialDrags[c.gameObject]) {
+                rb.drag += dragIncrease;
+            }
         }
     }
 
     void OnTriggerExit2D(Collider2D c) {
         if(c.gameObject.tag == "Trash") {
             Rigidbody2D rb = c.GetComponent<Rigidbody2D>();
-            rb.drag -= dragIncrease;
+            rb.drag = initialDrags[c.gameObject];
         }
     }
 }
