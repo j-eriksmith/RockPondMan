d79e09670d162b84d20954edf12c4d3f4ecd490b
jetsontwo <jetsontwo@gmail.com>
2017-01-21 11:47:32 -0800

    Added Secret Duck and Text Popup
    Allows players to click the duck to get access to secret duck levels

---
+++ Assets/Scripts/Animate_Text.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate_Text : MonoBehaviour {
+
+    public float speed;
+    private bool not_started = true;
+	
+    void Start()
+    {
+        transform.localScale = new Vector3(0.001f, 0.001f, 0.001f);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = Vector3.zero;
+        if(not_started)
+        {
+            StartCoroutine(Animate());
+            not_started = false;
+        }
+	}
+
+
+    IEnumerator Animate()
+    {
+        
+        while (transform.localScale.x < 1f && transform.localScale.y < 1f)
+        {
+            transform.localScale += new Vector3(speed, speed, 0);
+            yield return new WaitForSeconds(0.00001f);
+        }
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
+    }
+}
