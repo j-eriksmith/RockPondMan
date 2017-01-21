e8f50417e6519ac93732129ad414e173f662e338
tolkwes <tolkwes@gmail.com>
2017-01-20 21:46:14 -0800

    Piranah Code
    The piranah script and placeholder scenes and assets.

---
+++ Assets/PiranahMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiranahMovement : MonoBehaviour{
+    public GameObject location;
+
+    private bool hit;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        hit = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (hit == true)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, location.transform.position, 30 * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject.tag == "ripple")
+        {
+                hit = true;
+        }
+    }
+}
