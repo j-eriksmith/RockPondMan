e2cc81f3445922a8251dccd992377b0cb58f860d
leinerf <frenielzabala@gmail.com>
2017-01-22 11:45:30 -0800

    Screenshots

---
+++ Assets/Scripts/Duck_Unlock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duck_Unlock : MonoBehaviour {
+
+	public GameObject[] button;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+		if (PlayerPrefs.GetInt ("level_1") == 1)
+			button [0].SetActive (true);
+
+		if (PlayerPrefs.GetInt ("level_2") == 1)
+			button [1].SetActive (true);
+
+		if (PlayerPrefs.GetInt ("level_3") == 1)
+			button [2].SetActive (true);
+	}
+}
