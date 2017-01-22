614561cc0190472c4ac7fe599f2064fb18c6c18d
jetsontwo <jetsontwo@gmail.com>
2017-01-21 22:10:01 -0800

    Improves Level Transition and Fixed Ripple Error
    Made some text come up showing player they finished the level and plays
    a sound.  Also fixed an error where the player could not drop a rock in
    the ripples of another rock.

--- Assets/Scripts/Game_Manager.cs
+++ Assets/Scripts/Game_Manager.cs
@@ -2,26 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Game_Manager : MonoBehaviour {
 
     public int need_to_collect;
     public VacuumCollision vc;
     public start_to_level_selection level_select_script;
+    public Text Win_Text;
+    public Canvas canvas;
+    private bool won = false;
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.R))
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 
-        if (need_to_collect == vc.return_items_caught())
+        
+        if (need_to_collect == vc.return_items_caught() && !won)
+        {
             StartCoroutine(Win_Level());
-	}
+            won = true;
+        }
+    }
 
 
     IEnumerator Win_Level()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.5f);
+        Text win_text = Instantiate(Win_Text, canvas.transform);
+        win_text.transform.position = Vector3.zero;
+        yield return new WaitForSeconds(3);
         level_select_script.LoadOnClick(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
