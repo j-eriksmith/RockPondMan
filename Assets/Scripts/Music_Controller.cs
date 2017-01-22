7e589714b9bb52e5a027a8abb035d1617d888f89
jetsontwo <jetsontwo@gmail.com>
2017-01-22 11:46:25 -0800

    Updated Pause to Remove Rock and Made Music Persistent across Scenes

--- Assets/Scripts/Music_Controller.cs
+++ Assets/Scripts/Music_Controller.cs
@@ -6,27 +6,12 @@ public class Music_Controller : MonoBehaviour {
 
     public AudioSource music;
 	// Use this for initialization
-	void OnEnable () {
+	void Awake () {
         if (!music.isPlaying)
             music.Play();
-        else
-        {
-            print("start");
-            music.time = PlayerPrefs.GetFloat("music_time");
-            print(music.time);
-        }
+
+        DontDestroyOnLoad(this.gameObject);
+        
 	}
 
-    void Update()
-    {
-        PlayerPrefs.SetFloat("music_time", music.time);
-        print(music.time);
-    }
-	
-    void OnDisable()
-    {
-        print("end");
-        PlayerPrefs.SetFloat("music_time", music.time);
-        print(music.time);
-    }
 }
