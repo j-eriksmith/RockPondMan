598ccd8e6c3a67e57b7f660bc7a21b7092750f67
jetsontwo <jetsontwo@gmail.com>
2017-01-22 10:13:47 -0800

    Fixed Restart Pregab and Made Pause Script Stop Movement

---
+++ Assets/Scripts/Pause_Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause_Script : MonoBehaviour {
+
+    public GameObject[] list_of_moveing;
+    private Vector2[] movement_vectors;
+	
+    void OnEnable()
+    {
+        movement_vectors = new Vector2[list_of_moveing.Length];
+        for (int i = 0; i < list_of_moveing.Length; ++i)
+        {
+            Rigidbody2D rb2 = list_of_moveing[i].GetComponent<Rigidbody2D>();
+            movement_vectors[i] = rb2.velocity;
+            rb2.velocity = Vector2.zero;
+        }
+    }
+
+    void OnDisable()
+    {
+        for (int i = 0; i < movement_vectors.Length; ++i)
+            list_of_moveing[i].GetComponent<Rigidbody2D>().velocity = movement_vectors[i];
+    }
+}
