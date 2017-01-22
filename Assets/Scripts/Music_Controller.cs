60483b95955bd940615e4c433d3ff3df0fb273ab
jetsontwo <jetsontwo@gmail.com>
2017-01-22 10:58:05 -0800

    Pushed Music Holder and Debugging

---
+++ Assets/Scripts/Music_Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_Controller : MonoBehaviour {
+
+    public AudioSource music;
+	// Use this for initialization
+	void OnEnable () {
+        if (!music.isPlaying)
+            music.Play();
+        else
+        {
+            print("start");
+            music.time = PlayerPrefs.GetFloat("music_time");
+            print(music.time);
+        }
+	}
+
+    void Update()
+    {
+        PlayerPrefs.SetFloat("music_time", music.time);
+        print(music.time);
+    }
+	
+    void OnDisable()
+    {
+        print("end");
+        PlayerPrefs.SetFloat("music_time", music.time);
+        print(music.time);
+    }
+}
