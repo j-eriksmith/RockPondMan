f87153e0565b02a3528f8522843d9bb8ae845612
jetsontwo <jetsontwo@gmail.com>
2017-01-22 12:13:37 -0800

    Updated Levels for Music

--- Assets/Scripts/Music_Controller.cs
+++ Assets/Scripts/Music_Controller.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class Music_Controller : MonoBehaviour {
 
+    private static Music_Controller instance = null;
+    public static Music_Controller Instance
+    {
+        get { return instance; }
+    }
+
+
+
     public AudioSource music;
 	// Use this for initialization
 	void Awake () {
         if (!music.isPlaying)
             music.Play();
-
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        } else {
+            instance = this;
+        }
         DontDestroyOnLoad(this.gameObject);
-        
-	}
+
+    }
 
 }
