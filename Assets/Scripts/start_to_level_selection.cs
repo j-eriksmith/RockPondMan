212bead29d3337b754828f4b6644d59cb21e025a
leinerf <frenielzabala@gmail.com>
2017-01-21 21:09:25 -0800

    Level selection update

---
+++ Assets/Scripts/start_to_level_selection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class start_to_level_selection : MonoBehaviour {
+
+	// Use this for initialization
+
+
+	public void LoadOnClick(int level)
+	{
+		
+		Application.LoadLevel (level);
+	}
+
+}
